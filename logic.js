8ae6427a7187ee7e7ccd69c51b1403f651e7a988
Fondril <markojovanovic.q@yahoo.com>
2025-04-17 16:14:17 +0200

    Redesigned testimonials section

--- logic.js
+++ logic.js
@@ -125,6 +125,72 @@ function collapseParagraph(
   }
 }
 
+//// TESTIMONIALS CAROUSEL
+
+let currentTestimonialsIndex = 0;
+const testimonialsArrayList = document.querySelectorAll(".testimonial");
+const testimonialsArrayListLength = testimonialsArrayList.length - 1;
+const testimonialsContainer = document.getElementById(
+  "testimonials-section-testimonial-container"
+);
+function updateNewCarouselTracking() {
+  let targetId = "testimonial-circle" + currentTestimonialsIndex;
+  let target = document.getElementById(targetId);
+  target.classList.add("testimonials-section-slider-navigation-circles-active");
+}
+function updateOldCarouselTracking() {
+  let targetId = "testimonial-circle" + currentTestimonialsIndex;
+  let target = document.getElementById(targetId);
+  target.classList.remove(
+    "testimonials-section-slider-navigation-circles-active"
+  );
+}
+document
+  .getElementById("testimonials-section-slider-right")
+  .addEventListener("click", function (e) {
+    e.preventDefault(); // stop vertical scroll
+
+    updateOldCarouselTracking();
+    if (currentTestimonialsIndex < testimonialsArrayListLength) {
+      currentTestimonialsIndex++;
+    } else {
+      currentTestimonialsIndex = 0;
+    }
+
+    let targetId = "testimonial" + currentTestimonialsIndex;
+    const target = document.getElementById(targetId);
+
+    // Scroll horizontally to the target
+    testimonialsContainer.scrollTo({
+      left: target.offsetLeft,
+      behavior: "smooth",
+    });
+    updateNewCarouselTracking();
+  });
+document
+  .getElementById("testimonials-section-slider-left")
+  .addEventListener("click", function (e) {
+    e.preventDefault(); // stop vertical scroll
+
+    updateOldCarouselTracking();
+
+    if (currentTestimonialsIndex == 0) {
+      currentTestimonialsIndex = testimonialsArrayListLength;
+    } else {
+      currentTestimonialsIndex--;
+    }
+
+    let targetId = "testimonial" + currentTestimonialsIndex;
+    const target = document.getElementById(targetId);
+
+    // Scroll horizontally to the target
+    testimonialsContainer.scrollTo({
+      left: target.offsetLeft,
+      behavior: "smooth",
+    });
+    updateNewCarouselTracking();
+  });
+
 //// UPDATING FOOTER YEAR TO CURRENT
 
 const footerYearElement = document.getElementById("footer-year");
